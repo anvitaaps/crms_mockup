db93a2a2c373b68624f73562a878cd429b758c77
Anvita Dubey <anvitaaps@yahoo.comgit config --global user.email anvitaaps@yahoo.com>
2018-04-12 16:56:58 +0530

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,511 @@
+var patient = [{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarm : {
+		active : false,
+		message : 'Last alarm 10 min ago'
+	}
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarm : {
+		active : true,
+		message : 'Alarm Active',
+		parameter : 'hr',
+		conditions : ['Tachycardia', 'High BP', 'Low RR']
+	}
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarm : {
+		active : false,
+		message : 'No recent alarms'
+	}
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarm : {
+		active : false,
+		message : 'Last alarm 10 min ago'
+	}
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarm : {
+		active : false,
+		message : 'Last alarm 10 min ago'
+	}
+}]
+
+var alarms = [{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		},{
+			time : '08:52',
+			duration : '7 min',
+			parameter : 'Heart Rate',
+			average : '71',
+			peak : '97',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : 'Ongoing',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		},{
+			time : '08:52',
+			duration : '7 min',
+			parameter : 'Heart Rate',
+			average : '71',
+			peak : '97',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		},{
+			time : '08:52',
+			duration : '7 min',
+			parameter : 'Heart Rate',
+			average : '71',
+			peak : '97',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		},{
+			time : '09:10',
+			duration : '5 min',
+			parameter : 'Heart Rate',
+			average : '78',
+			peak : '92',
+		}
+	]
+},{
+	name : 'Haripad Bhatia',
+	sex : 'M',
+	age : 32,
+	bed : 1,
+	hr : 102,
+	rr : 48,
+	spo : 99,
+	temp : '98/102',
+	nibp : '112/88',
+	alarms : [
+		{
+			time : '10:45',
+			duration : '3 min',
+			parameter : 'Heart Rate',
+			average : '72',
+			peak : '99',
+		}
+	]
+}]
+
+$(document).ready(function(){
+	$.tmpl( $('#alarmTemplate').html(), alarms).prependTo( ".patientsContainer .alarms, .alarmsGroup" )
+
+	$.tmpl( $('#patientTemplate').html(), patient).prependTo( "#mainContainer .patientsList.urgent" )
+	$.tmpl( $('#patientTemplate').html(), patient).prependTo( "#mainContainer .patientsList.ward1" )
+	$.tmpl( $('#patientTemplate').html(), patient).prependTo( "#mainContainer .patientsList.ward2" )
+	$.tmpl( $('#patientTemplate').html(), patient).prependTo( "#mainContainer .patientsSearched .patientsList" )
+	$.tmpl( $('#patientTemplate').html(), patient).prependTo( "#mainContainer .patientsSearched .patientsList" )
+
+	$('.buttonLogin').bind('click', function(){
+		$('.loginMainContainer').hide();
+	})
+
+	$('.filtersPatients .optionValue').bind('click', function(){
+		$(this).parent().children('.optionValue').removeClass('active');
+		$(this).addClass('active');
+		if($(this).hasClass('default')){
+			$('.patientsContainer .patients').show();
+			$('.patientsContainer .patientsSearched').hide();
+		} else {
+			$('.patientsContainer .patients').hide();
+			$('.patientsContainer .patientsSearched').show();
+		}
+	})
+
+	$('.filtersAlarms .optionValue').bind('click', function(){
+		$(this).parent().children('.optionValue').removeClass('active');
+		$(this).addClass('active');
+
+		if($(this).hasClass('default')){
+			$('.patientsContainer .alarms').show();
+			$('.patientsContainer .alarmsSearched').hide();
+		} else if($(this).hasClass('filtered')){
+			$('.patientsContainer .alarms').hide();
+			$('.patientsContainer .alarmsSearched').show();
+		}
+	})
+
+	$('.patientSearch').bind('keyup', function(){
+		var val = $(this).val();
+		if(val == ''){
+			$('.patientsContainer .patients').show();
+			$('.patientsContainer .patientsSearched').hide();
+		} else {
+			$('.patientsContainer .patients').hide();
+			$('.patientsContainer .patientsSearched').show();
+		}
+	})
+
+	$('.historyOptions .option').bind('click', function(){
+		$('.historyOptions .option').removeClass('active');
+		$(this).addClass('active');
+	})
+
+	$('.historyOptions .option.alarmHistory').bind('click', function(){
+		$('.historyOptionDetails').removeClass('active');
+		$('.historyOptionDetails.alarmHistory').addClass('active');
+	})
+
+	$('.historyOptions .option.dataTable').bind('click', function(){
+		$('.historyOptionDetails').removeClass('active');	
+		$('.historyOptionDetails.dataTable').addClass('active');
+	})
+
+	$('.historyOptions .option.trendGraphs').bind('click', function(){
+		$('.historyOptionDetails').removeClass('active');
+		$('.historyOptionDetails.trendGraphs').addClass('active');
+	})
+
+	$('.patientContainer').bind('click', function(){
+		$('#mainContainer').addClass('showingDetails');
+	})
+
+	$('.patientDetails .buttonBack').bind('click', function(){
+		$('#mainContainer').removeClass('showingDetails');
+	})
+
+	$('.otherOptions .option').bind('click', function(){
+
+		if($(this).hasClass('open')){
+			$(this).removeClass('open');
+		} else {
+			$('.otherOptions .option').removeClass('open');
+			$(this).addClass('open');
+		}
+	})
+
+	$('.tabPatients').bind('click', function(){
+		$('.tabPatients').addClass('active');
+		$('.tabAlarms').removeClass('active');
+		$('.patientsContainer .alarms').hide();
+		$('.patientsContainer .alarmsSearched').hide();
+		$('.patientsContainer .patients').show();
+
+		$('.patientsContainer .filtersAlarms').hide();
+		$('.patientsContainer .filtersPatients').show();
+	})
+
+	$('.tabAlarms').bind('click', function(){
+		$('.tabPatients').removeClass('active');
+		$('.tabAlarms').addClass('active');
+		$('.patientsContainer .alarms').show();
+		$('.patientsContainer .patients').hide();
+		$('.patientsContainer .patientsSearched').hide();
+
+		$('.patientsContainer .filtersAlarms').show();
+		$('.patientsContainer .filtersPatients').hide();
+	})
+
+	$('.patientDetailsContainer .action.history').bind('click', function(){
+		$('.patientDetailsContainer .action.vitals').removeClass('active');
+		$('.patientDetailsContainer .action.settings').removeClass('active');
+		$(this).addClass('active');
+		$('.patientDetailsContainer .patientHistory').show();
+		$('.patientDetailsContainer .patientLiveData').hide();
+		$('.patientDetailsContainer .patientSettings').hide();
+	})
+
+	$('.patientDetailsContainer .action.vitals').bind('click', function(){
+		$('.patientDetailsContainer .action.history').removeClass('active');
+		$('.patientDetailsContainer .action.settings').removeClass('active');
+		$(this).addClass('active');
+		$('.patientDetailsContainer .patientHistory').hide();
+		$('.patientDetailsContainer .patientLiveData').show();
+		$('.patientDetailsContainer .patientSettings').hide();
+	})
+
+	$('.patientDetailsContainer .action.settings').bind('click', function(){
+		$('.patientDetailsContainer .action.history').removeClass('active');
+		$('.patientDetailsContainer .action.vitals').removeClass('active');
+		$(this).addClass('active');
+
+		$('.patientDetailsContainer .patientHistory').hide();
+		$('.patientDetailsContainer .patientLiveData').hide();
+		$('.patientDetailsContainer .patientSettings').show();
+	})
+
+	$('.iconNotification').bind('click', function(){
+		$('.notificationsList').show();
+		$('.iconNotification .counter').hide();
+
+		$('.iconNotification').unbind().bind('click', function(){
+			$('.notificationsList').hide();
+		})
+	})
+
+	$('.notifications .buttonClear').bind('click', function(){
+		$('.notificationSingle').hide();
+		$('.noNotifications').show();
+		$(this).addClass('disable');
+	})
+
+})
